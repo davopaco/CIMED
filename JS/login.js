d1f0c168b03b933cf43f86871cb6fcf93468280d
David Cárdenas <santito500@live.com>
2023-09-02 12:32:49 -0500

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -6,18 +6,18 @@ const app =express();
 const connection = database.con;
 
 app.post("/login", (req, res) => {
-    const { username, password } = req.body;
+    const { id, password } = req.body;
   
     // Check if the username exists
     connection.query(
       "SELECT * FROM PACIENTE WHERE ID = ?",
-      [username],
+      [id],
       (err, rows) => {
         if (err) {
           console.log(err);
           res.status(500).send("Error");
         } else if (rows.length === 0) {
-          res.status(401).send("Username does not exist");
+          res.status(401).send("El usuario no existe");
         } else {
           // Check if the password is correct
           const hashedPassword = rows[0].password;
