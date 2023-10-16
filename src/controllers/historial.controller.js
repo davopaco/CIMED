a0ac3df0fa6314152e6427c556e486dbd2e7d78d
David Cárdenas <santito500@live.com>
2023-10-16 17:28:57 -0500

    Submit #8
    Algunos arreglos del HTML de historial

--- src/controllers/historial.controller.js
+++ src/controllers/historial.controller.js
@@ -4,46 +4,45 @@ import { fileURLToPath } from "url";
 
 export const agregarHistorial = async (req, res) => {
   const {
-    fecha_form,
-    pasado_med,
+    fecha,
     sintomas,
-    historial_familiar,
-    historial_med,
-    alergias,
+    examen,
     resultados,
-    examen_fis,
-    codigo_medicamento,
-    receta_med_bool,
+    alergias,
+    ante_med,
+    ante_fam,
+    medic_ant,
+    paciente,
+    receta_medica,
   } = req.body;
-  const id_cita = req.query.id_cita;
-
-  const [rows1] = await pool.query(
-    "SELECT id_medico, paciente_id FROM CITA WHERE ID = ?",
+  const id_cita = paciente;
+  const [rows] = await pool.query(
+    "SELECT id_medico, paciente_id FROM CITA WHERE ID =?",
     [id_cita]
   );
-  const id_medico = rows1[0].id_medico;
-  const id_paciente = rows1[0].paciente_id;
+  const id_medico = rows[0].id_medico;
+  const id_paciente = rows[0].paciente_id;
 
   const [historiales] = await pool.query(
     "INSERT INTO HISTORIAL_CLINICO (FECHA_FORM, PASADO_MED, SINTOMAS, HISTORIAL_FAMILIAR, HISTORIAL_MEDICACION, ALERGIAS, RESULTADOS_LAB, EXAMEN_FIS, PACIENTE_ID, PROFESIONAL_SALUD_ID) VALUES (?,?,?,?,?,?,?,?,?,?)",
     [
-      fecha_form,
-      pasado_med,
+      fecha,
+      ante_med,
       sintomas,
-      historial_familiar,
-      historial_med,
+      ante_fam,
+      medic_ant,
       alergias,
       resultados,
-      examen_fis,
+      examen,
       id_paciente,
       id_medico,
     ]
   );
 
   var recetas;
-  if (receta_med_bool == true) {
+  if (receta_medica == true) {
     [recetas] = await pool.query(
-      "INSERT INTO RECETA_MEDICA (ID, CODIGO_MEDICAMENTO, PROFESIONAL_SALUD_ID, HISTORIAL_CLINICO_ID, PACIENTE_ID) VALUES (?,?,?,?,?)",
+      "INSERT INTO RECETA_MEDICA (CODIGO_MEDICAMENTO, PROFESIONAL_SALUD_ID, HISTORIAL_CLINICO_ID, PACIENTE_ID) VALUES (?,?,?,?,?)",
       [fecha_form, pasado_med, sintomas, historial_clinico]
     );
   }
@@ -67,6 +66,17 @@ export const getHistorial = async (req, res) => {
   res.render("historiales", { historiales });
   res.render("recetas", { recetas });
 };
-export const defaultR = (req, res) => {
-  res.render("Historial.ejs");
+
+export const defaultR = async (req, res) => {
+  const [rows1] = await pool.query(
+    "SELECT p.nombre, c.id, fecha_asig FROM PACIENTE p, CITA c WHERE c.paciente_id = p.id"
+  );
+  const codigosMed = ["INF123", "MED456", "ING789"];
+  const meds = [
+    "Acetaminofén 500 mg",
+    "Acido Bórico 5 G",
+    "Acetato de Aluminio",
+  ];
+  const data = rows1;
+  res.render("Historial.ejs", { data, codigosMed, meds });
 };
