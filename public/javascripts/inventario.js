ef7fb678000af85e419158b999316e819310c341
Sofía princess <sofusaka@gmail.com>
2023-10-17 14:35:53 -0500

    Commit #3
    Search bar permite meter dependiendo de la categoría

--- public/javascripts/inventario.js
+++ public/javascripts/inventario.js
@@ -1,21 +1,33 @@
-    document.addEventListener("DOMContentLoaded", function () {
-        const searchBox = document.getElementById("search-box"),
-            tablaInv = document.getElementById("table-inv"),
-            buttSearch = document.getElementById("button-search");
-            rows = tablaInv.getElementsByClassName("filterable-row");
+document.addEventListener("DOMContentLoaded", function () {
+    const searchBox = document.getElementById("search-box"),
+        tablaInv = document.getElementById("table-inv"),
+        selectInv = document.getElementById("category-select");
+    rows = tablaInv.getElementsByClassName("filterable-row");
+    selectInv.addEventListener("change", function () {
+        valueOfSelect = selectInv.value;
+        if (valueOfSelect == 0) {
+            console.log("Disabled")
+            searchBox.disabled = true;
+        } else {
+            console.log("Not disabled")
+            searchBox.disabled = false;
+        }
+    });
+    searchBox.disabled = true;
 
-        searchBox.addEventListener("input", function () {
-            const query = searchBox.value.toLowerCase();
 
-            Array.from(rows).forEach(function (row) {
-                const idCell = row.querySelector("td:nth-child(2)");
-                const id = idCell.textContent.toLowerCase();
+    searchBox.addEventListener("input", function () {
+        const query = searchBox.value.toLowerCase();
 
-                if (id.includes(query)) {
-                    row.style.display = "table-row";
-                } else {
-                    row.style.display = "none";
-                }
-            });
+        Array.from(rows).forEach(function (row) {
+            const idCell = row.querySelector(`td:nth-child(${valueOfSelect})`);
+            const id = idCell.textContent.toLowerCase();
+
+            if (id.includes(query)) {
+                row.style.display = "table-row";
+            } else {
+                row.style.display = "none";
+            }
         });
     });
+});
