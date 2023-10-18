9973f2443d9f4060a4ca445e1355a63c4825184a
David Cárdenas <santito500@live.com>
2023-10-17 22:42:55 -0500

    Commit #1
    Vista cambiar usuario ya quedó lista

--- src/controllers/cambioUsuario.controller.js
+++ src/controllers/cambioUsuario.controller.js
@@ -1,8 +1,41 @@
-export const cambioUsuarioPage = (req, res) => {
+import { pool } from "../database.js";
+
+export const cambioUsuarioPage = async (req, res) => {
   const dataSend = {
     id: req.query.id,
     page: req.query.page,
   };
+  const { nombre, referencia_pfp } = await consultaDB(
+    dataSend.id,
+    dataSend.page
+  );
+  dataSend.nombre = nombre;
+  dataSend.refPfp = referencia_pfp;
   console.log(dataSend);
   res.render("cambio_usuario.ejs", { dataSend });
 };
+
+async function consultaDB(id, page) {
+  if (page % 2 != 0) {
+    const [rows] = await pool.query(
+      "SELECT nombre, referencia_pfp FROM PACIENTE WHERE ID = ?",
+      [id]
+    );
+    console.log(rows[0]);
+    return rows[0];
+  }
+  if (page == 3 || page == 6) {
+    const [rows] = await pool.query(
+      "SELECT nombre, referencia_pfp FROM PROFESIONAL_SALUD WHERE ID = ?",
+      [id]
+    );
+    return rows[0];
+  }
+  if (page == 5) {
+    const [rows] = await pool.query(
+      "SELECT nombre, referencia_pfp FROM LOGISTICA WHERE ID = ?",
+      [id]
+    );
+    return rows[0];
+  }
+}
